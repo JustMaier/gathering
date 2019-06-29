a856723c9214cc033dd190b81787c3b947eb98dd
Justin Maier <Zipp425@gmail.com>
2019-06-29 14:26:12 +0200

    Oopsie swarmsie

--- src/db/GatheringDB.js
+++ src/db/GatheringDB.js
@@ -64,7 +64,7 @@ class GatheringDB extends EventEmitter {
       config: {
         Addresses: {
           Swarm: [
-            // signalServer
+            signalServer
           ]
         },
         Bootstrap: []
